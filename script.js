7f64b7d92e423f9964404ba1e44d4b82eb869bd5
sheryiscool <liveclass@sheryians.org>
2023-07-05 21:24:09 +0530

    Added animations and fonts

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-var moon = document.querySelector("#moon")
-var flag = 0
-moon.addEventListener("click", function () {
-    if (flag == 0) {
-        document.documentElement.style.setProperty("--pri", "#fff")
-        document.documentElement.style.setProperty("--sec", "#000")
-        moon.innerHTML = `<i class="ri-sun-fill"></i>`
-        flag = 1
-    }else{
-        document.documentElement.style.setProperty("--pri", "#000")
-        document.documentElement.style.setProperty("--sec", "#fff")
-        moon.innerHTML = `<i class="ri-moon-fill"></i>`
-        flag = 1
-    }
+var tl = gsap.timeline()
+
+tl.from("#nav h1,#nav h4,#nav i,#nav h5",{
+    delay:0.5,
+    y:-30,
+    stagger:0.1,
+    opacity:0
+})
+.from("#center>h1",{
+    opacity:0,
+    y:40,
+    scale:1.2,
+    duration:0.6
+})
+.from("#center>p",{
+    opacity:0,
+    y:20,
+    duration:0.3
+})
+.from("#page1 img",{
+    scale:0.7,
+    opacity:0,
+    duration:0.8
 })
